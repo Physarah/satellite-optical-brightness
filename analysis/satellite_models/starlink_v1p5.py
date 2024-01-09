4efe0fabd8a27e3338b6e0df5f8c5b8f0a2f73a1
Sarah caddy <sarah.caddy@bigpond.com>
2024-01-09 17:02:12 +1100

    testing

--- analysis/satellite_models/starlink_v1p5.py
+++ analysis/satellite_models/starlink_v1p5.py
@@ -10,9 +10,10 @@ import numpy as np
 # BRDFs inferred from on-orbit brightness observations.
 
 # Constants
-chassis_area = 3.65 # m^2
-solar_array_area = 22 # m^2
+chassis_area = 3.65  # m^2
+solar_array_area = 22  # m^2
 
+#chassis_normal = np.array([0, 0, -1])
 chassis_normal = np.array([0, 0, -1])
 solar_array_normal = np.array([0, 1, 0])
 
@@ -22,12 +23,13 @@ solar_array_normal = np.array([0, 1, 0])
 # The script for fitting is "lab_brdf_fits.ipynb"
 
 B = np.array([[3.34, -98.085]])
-C = np.array([[-999.999, 867.538, 1000., 1000., -731.248, 618.552, -294.054, 269.248, -144.853, 75.196]])
-lab_chassis_brdf = BINOMIAL(B, C, d = 3.0, l1 = -5)
+C = np.array([[-999.999, 867.538, 1000., 1000., -731.248,
+               618.552, -294.054, 269.248, -144.853, 75.196]])
+lab_chassis_brdf = BINOMIAL(B, C, d=3.0, l1=-5)
 
 B = np.array([[0.534, -20.409]])
 C = np.array([[-527.765, 1000., -676.579, 430.596, -175.806, 57.879]])
-lab_solar_array_brdf = BINOMIAL(B, C, d = 3.0, l1 = -3)
+lab_solar_array_brdf = BINOMIAL(B, C, d=3.0, l1=-3)
 
 SURFACES_LAB_BRDFS = [
     Surface(chassis_area, chassis_normal, lab_chassis_brdf),
